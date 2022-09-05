6e59155463e212ae598891a73c83e104e9b8d4fa
wavesaround <artem.mustaev@gmail.com>
2022-09-05 14:57:56 +0300

    Task 15 complete

---
+++ Class 2 HM/Task 15_day_off/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число от 1 до 7: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void day()
+{
+    if (n > 0 && n < 6) Console.WriteLine("Нет");
+    else Console.WriteLine("Да");
+}
+
+if (n < 1 || n > 7) Console.WriteLine("Число введено неверно");
+else day();
