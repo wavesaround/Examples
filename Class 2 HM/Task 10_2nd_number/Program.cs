c6ec4d78797476c1fa80b11fad9191c4682154ba
wavesaround <artem.mustaev@gmail.com>
2022-09-05 13:10:30 +0300

    task 10 complete

---
+++ Class 2 HM/Task 10_2nd_number/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void CompreiseNumbers()
+{
+    int a = n % 100;
+    Console.WriteLine(a / 10);
+}
+
+if (n > 99 && n < 1000) CompreiseNumbers();
+
+else Console.WriteLine("Вы ввели неверное значение!");
