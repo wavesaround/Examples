ac72612394b758336861deee1a5140a9746b9012
wavesaround <artem.mustaev@gmail.com>
2022-09-05 14:42:56 +0300

    Task 13 complete

---
+++ Class 2 HM/Task 13_3rd_number/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет */
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void number()
+{
+    if (n > 99)
+    {
+
+        while (n > 999)
+        {
+            n = n / 10;
+        }
+        Console.WriteLine(n % 10);
+    }
+
+    else Console.WriteLine("Третьей цифры нет ");
+}
+
+number();
