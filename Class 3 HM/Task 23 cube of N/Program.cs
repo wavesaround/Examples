318e65cc7db13a07e5ce5376fcb861b97a5a9a9d
wavesaround <artem.mustaev@gmail.com>
2022-09-08 14:18:18 +0300

    Task 23 comlete

---
+++ Class 3 HM/Task 23 cube of N/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+if (n <= 0) Console.WriteLine("Введите число больше нуля");
+
+while (i <= n)
+{
+    Console.Write(Math.Pow(i, 3) + ", ");
+    i++;
+}
