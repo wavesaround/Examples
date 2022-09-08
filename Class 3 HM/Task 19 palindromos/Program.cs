f3064bde8549b69a35e4893ecd15114a769891c7
wavesaround <artem.mustaev@gmail.com>
2022-09-08 13:17:26 +0300

    Task 19 complete

---
+++ Class 3 HM/Task 19 palindromos/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите 5-значное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Palindrom()
+{
+    int a = 0;
+    int b = 0;
+    a = n / 1000;
+    b = n % 100;
+    b = (b % 10) * 10 + (b / 10);
+
+    if (a == b)
+        Console.WriteLine("Да");
+    else
+        Console.WriteLine("Нет");
+}
+
+if (n < 10000 || n > 99999) Console.WriteLine("Значение не соответствует условию");
+else Palindrom();
