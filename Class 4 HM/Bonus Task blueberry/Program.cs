430c6018eca0dda973710764a8923de0871a2e67
wavesaround <artem.mustaev@gmail.com>
2022-09-09 14:02:44 +0300

    Bonus Task Blueberry complete

---
+++ Class 4 HM/Bonus Task blueberry/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+int[] bb1 = { 1, 2, 3, 4 };
+int[] bb2 = { 1, 2, 3 };
+int[] bb3 = { 5, 8, 3, 5, 1, 9, 7 };
+
+void blueberry(int[] array)
+{
+    int comp = 0;
+
+    for (int i = 1; i < array.Length - 1; i++)
+    {
+        if ((array[i] + array[i - 1] + array[i + 1]) > comp) 
+            comp = array[i] + array[i - 1] + array[i + 1];
+    }
+    Console.WriteLine(comp);
+}
+
+blueberry(bb1);
