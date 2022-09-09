bdc66f47967ce9a452ed90c0da776d9a253d270f
wavesaround <artem.mustaev@gmail.com>
2022-09-09 12:46:28 +0300

    Task 25 complete

---
+++ Class 4 HM/Task 25 A-B/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine($"Результат: {Math.Pow(a, b)}");
