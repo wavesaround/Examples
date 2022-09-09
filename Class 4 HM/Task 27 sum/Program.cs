ea5b0887856b644139965492afbd525fc3d053f7
wavesaround <artem.mustaev@gmail.com>
2022-09-09 13:05:20 +0300

    Task 27 complete

---
+++ Class 4 HM/Task 27 sum/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (n != 0)
+{
+    sum += (n % 10);  
+    n /= 10;         
+}
+
+Console.Write(sum);
