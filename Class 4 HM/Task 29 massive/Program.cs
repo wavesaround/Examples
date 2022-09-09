28081920691aedc599b2672c6e1f2cba80caafa8
wavesaround <artem.mustaev@gmail.com>
2022-09-09 13:18:26 +0300

    Task 29 complete

---
+++ Class 4 HM/Task 29 massive/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
