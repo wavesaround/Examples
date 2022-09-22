359c928abc9ab3217a79f3db3a5dbaf413b312b4
wavesaround <artem.mustaev@gmail.com>
2022-09-22 21:06:58 +0300

    task 52 completed

---
+++ Class 7 HM/Task 52/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. 
+*/
+
+Console.Clear();
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindAverage(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            sum += matrix[j, i];
+        }
+        Console.Write(Math.Round(sum / matrix.GetLength(0), 3) + " \t");
+    }
+}
+
+int[,] matrix = new int[4, 4];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+FindAverage(matrix);
