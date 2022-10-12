4a5b5d57b7538f6954ead59a047b5aa989e2e0f5
wavesaround <artem.mustaev@gmail.com>
2022-10-12 13:47:54 +0300

    Task 54 completed

---
+++ Class 8 HM/Task 54/Program.cs
@@ -0,0 +1,61 @@
+﻿/* Задайте двумерный массив. 
+Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива. */
+
+Console.Clear();
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Sort(int[,] matrix)
+{
+    
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int times = 0;
+        while (times < matrix.GetLength(1))
+        {
+            int pos = 0;
+            int MaxValue = 0;
+            for (int j = times; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[i, j] > MaxValue)
+                {
+                    MaxValue = matrix[i, j];
+                    pos = j;
+                }
+            }
+            matrix[i, pos] = matrix[i, times];
+            matrix[i, times] = MaxValue;
+            times++;
+        }
+    }
+}
+
+int[,] matrix = new int[4, 4];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+Sort(matrix);
+PrintMatrix(matrix);
