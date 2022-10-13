e39ecff3b368185a18d617b84784da26b59b3de9
wavesaround <artem.mustaev@gmail.com>
2022-10-13 13:23:04 +0300

    Task 60 without repeat check

---
+++ Class 8 HM/Task 60/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента. */
+
+int[,,] InputMatrix(int row, int col)
+{
+    int[,,] matrix = new int[row, col, col];
+    int[] CheckMatrix = new int[2 * (row + col)];
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[j, k, i] = new Random().Next(11, 99);
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[j, k, i]}  ({j}, {k}, {i})  ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int col = Convert.ToInt32(Console.ReadLine());
+
+int[,,] matrix = InputMatrix(row, col);
+PrintMatrix(matrix);
